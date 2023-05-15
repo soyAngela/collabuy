b834ca9f4d67a2dad2aa0ad12308113e69878683
soyAngela <angelagonzalez1999@hotmail.es>
2023-05-15 19:17:08 +0200

    Infraestructura de InicioSesion y Registro

--- app/src/main/java/com/example/collabuy/InicioSesion.java
+++ app/src/main/java/com/example/collabuy/InicioSesion.java
@@ -2,7 +2,14 @@ package com.example.collabuy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class InicioSesion extends AppCompatActivity {
 
@@ -10,5 +17,29 @@ public class InicioSesion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_sesion);
+
+        Activity actividadLogin = this;
+
+        TextView textRegistrate = findViewById(R.id.textRegistrate);
+        EditText editUsuario = findViewById(R.id.editUsuario);
+        EditText editContrasena = findViewById(R.id.editContrasena);
+        Button botonIniciar = findViewById(R.id.botonIniciar);
+
+        textRegistrate.setPaintFlags(textRegistrate.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+
+        textRegistrate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(InicioSesion.this, Registro.class);
+                actividadLogin.startActivityForResult(intent, 1);
+            }
+        });
+
+        botonIniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO: Resultado del boton de iniciar sesion
+            }
+        });
     }
 }
