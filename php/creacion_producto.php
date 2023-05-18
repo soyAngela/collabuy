552faf31ea233698252f735d855af1d707381599
hugof <hugofa4@gmail.com>
2023-05-18 19:06:01 +0200

    Creacion producto

--- php/creacion_producto.php
+++ php/creacion_producto.php
@@ -5,15 +5,14 @@ include 'conexion_collabuy.php';
 $nombre = $_POST["nombre"];
 $descripcion = $_POST["descripcion"];
 $imagen = $_POST["imagen"];
-$usuario = $_["usuario"];
+$usuario = $_POST["usuario"];
 
-if($metodo == 0){
-    # Ejecutar la sentencia SQL de inserción de producto
-    $resultado = mysqli_query($con, "INSERT INTO Producto (nombre,descripcion,imagen,creador) VALUES('$nombre','$descripcion','$imagen','$usuario')");
-    # Comprobar si se ha ejecutado correctamente
-    if (!$resultado) {
-        echo 'Ha ocurrido algún error: ' . mysqli_error($con);
-    }
+
+# Ejecutar la sentencia SQL de inserción de producto
+$resultado = mysqli_query($con, "INSERT INTO Producto (nombre,descripcion,imagen,creador) VALUES('$nombre','$descripcion','$imagen','$usuario')");
+# Comprobar si se ha ejecutado correctamente
+if (!$resultado) {
+    echo 'Ha ocurrido algún error: ' . mysqli_error($con);
 }
 
 mysqli_close($con);
