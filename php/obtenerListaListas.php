82d510975ae0d76a70d6fcc1b0055b0f979468b2
Lucas <lucas00abaitua@gmail.com>
2023-05-18 17:21:14 +0200

    Commit de subida php

---
+++ php/obtenerListaListas.php
@@ -0,0 +1,25 @@
+<?php
+include 'conexion_collabuy.php';
+
+$parametro = $_POST["usuario"];
+
+# Ejecutar la sentencia SQL
+$resultado = mysqli_query($con, "SELECT * FROM Lista INNER JOIN Participacion WHERE id_usuario = '$parametro'");
+
+# Comprobar si se ha ejecutado correctamente
+if (!$resultado) {
+    echo 'Ha ocurrido algun error: ' . mysqli_error($con);
+}
+
+#Acceder al resultado
+$fila = mysqli_fetch_row($resultado);
+
+# Generar el array con los resultados con la forma Atributo - Valor
+$arrayresultados = array('id' => $fila[0], 'nombre' => $fila[1], 'clave' => $fila[2]);
+
+#Devolver el resultado en formato JSON
+$res = json_encode($arrayresultados);
+echo $res;
+
+close();
+?>
