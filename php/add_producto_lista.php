6e485b3d7218164b2f7f8c28bd118c81d83575e2
AArana670 <76699832+AArana670@users.noreply.github.com>
2023-05-23 00:17:40 +0200

    Ficheros PHP actualizados con respecto al servidor

---
+++ php/add_producto_lista.php
@@ -0,0 +1,22 @@
+<?php
+
+#include 'conexion_collabuy.php';
+include 'creacion_producto.php';
+
+echo $res;
+$data = json_decode($res, true);
+$idProd = $data['id'];
+$idLista = $data['idLista'];
+
+$resultado = mysqli_query($con, "INSERT INTO Contiene (id_lista, id_producto, cantidad, comprado) VALUES ('$idLista', '$idProd', 1, false)");
+
+# Comprobar si se ha ejecutado correctamente
+if ($resultado) {
+    echo '1';
+}else{
+    echo 'Ha habido un error: ' . mysqli_error($con);
+}
+
+mysqli_close($con);
+
+?>
--- php/creacion_producto.php
+++ php/creacion_producto.php
@@ -6,7 +6,7 @@ $nombre = $_POST["nombre"];
 $descripcion = $_POST["descripcion"];
 $imagen = $_POST["imagen"];
 $usuario = $_POST["usuario"];
-
+$idLista = $_POST["idLista"];
 
 # Ejecutar la sentencia SQL de inserción de producto
 $resultado = mysqli_query($con, "INSERT INTO Producto (nombre,descripcion,imagen,creador) VALUES('$nombre','$descripcion','$imagen','$usuario')");
@@ -15,5 +15,21 @@ if (!$resultado) {
     echo 'Ha ocurrido algún error: ' . mysqli_error($con);
 }
 
-mysqli_close($con);
+$id = mysqli_query($con, "SELECT id FROM Producto WHERE creador = '$usuario' ORDER BY id DESC LIMIT 1");
+if (!$id) {
+    echo 'Ha ocurrido algún error: ' . mysqli_error($con);
+}
+$arrayresultados = array();
+#Construir la lista elemento a elemento
+while ($fila = mysqli_fetch_assoc($id)) {
+    $arrayresultados = ['id' => $fila['id'], 'idLista' => $idLista];
+}
+
+#Devolver el resultado en formato JSON
+$res = json_encode($arrayresultados);
+
+echo $res;
+#echo ($id);
+
+#mysqli_close($con);
 ?>
